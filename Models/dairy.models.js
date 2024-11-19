6e6f10694915b59452ae9a96fdf8ce4cc805c23f
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-19 17:00:36 +0530

    "Added DiarySchema to dairy.models.js and removed empty lines from app.js"

---
+++ Models/dairy.models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const DiarySchema = new mongoose.Schema({
+    heading:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    createdBy:{
+        type:String,
+        required:false
+    }
+},{
+    timestamps:true
+})
+
+module.exports = DiarySchema
--- app.js
+++ app.js
@@ -11,12 +11,6 @@ app.get('/home',(req,res)=>{
     res.send('<h1>This is the home page</h1>')
 })
 
-
-
-
-
-
-
 const port = process.env.PORT || 3000;
 connectDB(process.env.MONGO_URI).then(() => {
     console.log('Database connected Sucessfully!')
