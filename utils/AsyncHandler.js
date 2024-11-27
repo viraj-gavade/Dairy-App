2efa5cb2d9aa6d14e7f212edfcb43dc02eed1dfb
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-27 10:34:34 +0530

    "Added JSDoc comments to AsyncHandler.js and reformatted code"

--- utils/AsyncHandler.js
+++ utils/AsyncHandler.js
@@ -1,8 +1,15 @@
-const asyncHandler = (fn)=>{
-    return  (req,res,next)=>{
-          Promise.resolve( fn(req,res,next)).
-          catch((err)=>next(err))
-      }
-      }
-
-module.exports =  asyncHandler
+/**
+* Async Error Handling Middleware
+* Wraps async route handlers to catch and forward errors
+* 
+* @param {Function} fn - Async route handler function
+* @returns {Function} Middleware function
+*/
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch((err) => next(err))
+    }
+ }
+ 
+ module.exports = asyncHandler
